549b3954c12c3eac8f3066e2fff0a77e2a93128d
Marcin Prokop <marcin@prokop.co>
2017-08-24 21:30:36 +0200

    Add AttachEnvGroup method API

--- lib/jelastic/rest/control.rb
+++ lib/jelastic/rest/control.rb
@@ -1,6 +1,13 @@
 module Jelastic
   module REST
     module Control
+      def attach_env_group(env_name:, env_group: nil, env_groups: nil)
+        send_request(
+          'environment/control/rest/attachenvgroup',
+          envName: env_name, envGroup: env_group, envGroups: env_groups
+        )
+      end
+
       def create_environment(params = {})
         params = to_json(params, :env)
         params = to_json(params, :nodes)
@@ -11,21 +18,21 @@ module Jelastic
       def delete_environment(env_name)
         send_request(
           'environment/control/rest/deleteenv',
-          { envname: env_name, password: self.password }
+          envname: env_name, password: password
         )
       end
 
       def add_docker_volume(env_name:, node_id:, path:)
         send_request(
           'environment/control/rest/adddockervolume',
-          { envname: env_name, nodeId: node_id, path: path }
+          envname: env_name, nodeId: node_id, path: path
         )
       end
 
       def add_docker_volume_by_group(env_name:, group_id:, path:)
         send_request(
           'environment/control/rest/adddockervolumebygroup',
-          { envname: env_name, groupId: group_id, path: path }
+          envname: env_name, groupId: group_id, path: path
         )
       end
 
@@ -34,19 +41,19 @@ module Jelastic
 
         send_request(
           'environment/control/rest/setdockerenvvars',
-          { envname: env_name, nodeId: node_id, data: data }
+          envname: env_name, nodeId: node_id, data: data
         )
       end
 
       def get_docker_env_vars(env_name:, node_id:)
         send_request(
           'environment/control/rest/getdockerenvvars',
-          { envname: env_name, nodeId: node_id }
+          envname: env_name, nodeId: node_id
         )
       end
 
       def get_environment(env_name)
-        send_request('environment/control/rest/getenvinfo', { envname: env_name })
+        send_request('environment/control/rest/getenvinfo', envname: env_name)
       end
 
       def get_environments
@@ -56,21 +63,21 @@ module Jelastic
       def redeploy_containers_by_group(env_name:, node_group:, tag:, sequential: false, use_existing_volumes: false)
         send_request(
           'environment/control/rest/redeploycontainersbygroup',
-          { envname: env_name, nodeGroup: node_group, tag: tag, sequential: sequential, useExistingVolumes: use_existing_volumes }
+          envname: env_name, nodeGroup: node_group, tag: tag, sequential: sequential, useExistingVolumes: use_existing_volumes
         )
       end
 
       def redeploy_container_by_id(env_name:, node_id:, tag:, use_existing_volumes: false)
         send_request(
           'environment/control/rest/redeploycontainerbyid',
-          { envname: env_name, nodeId: node_id, tag: tag, useExistingVolumes: use_existing_volumes }
+          envname: env_name, nodeId: node_id, tag: tag, useExistingVolumes: use_existing_volumes
         )
       end
-      
+
       def restart_container_by_id(env_name:, node_id:)
         send_request(
           'environment/control/rest/restartcontainerbyid',
-          { envname: env_name, nodeId: node_id }
+          envname: env_name, nodeId: node_id
         )
       end
     end
